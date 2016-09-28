a417f846c364bb0b970e35161085bf547040df90
Luke <lukeaveil@yahoo.com>
2016-09-28 16:33:48 +0100

    adds logic for sidebar form

--- controllers/classifieds.ctr.js
+++ controllers/classifieds.ctr.js
@@ -8,12 +8,27 @@
         $scope.classifieds = classifieds.data;
       });
 
+      var contact = {
+        name: "luke",
+        phone: "98765",
+        email: "lukeaveil@yahoo.com"
+      };
+
       $scope.openSidebar = function() {
         $mdSidenav('left').open();
       };
 
       $scope.closeSidebar = function() {
         $mdSidenav('left').close();
+      };
+
+      $scope.saveClassified = function(classified) {
+        if(classified) {
+          classified.contact = contact;
+          $scope.classifieds.push(classified);
+          $scope.classified = {};
+          $scope.closeSidebar();
+        }
       };
 
     });
